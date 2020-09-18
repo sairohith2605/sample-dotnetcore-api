f4ae7f92983216292e2333207bf8212b1acf62de
Sai Rohith <sairohith2605@outlook.com>
2020-09-18 19:36:42 +0530

    Add project files.

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APT_Software_API.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APT_Software_API.Controllers
+{
+    [Route("sampleapi")]
+    [ApiController]
+    [EnableCors]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet("text")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetSampleMessage()
+        {
+            return new OkObjectResult(new GenericResponse() { Message = "This is a sample response!" });
+        }
+    }
+}
---
+++ Models/GenericResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace APT_Software_API.Models
+{
+    public class GenericResponse
+    {
+        public string Message { get; set; }
+    }
+}
